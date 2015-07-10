3db3ac52d9af383cd253d4060f65d85af7d211ed
Janne Aalto <janne.aalto@ddb.fi>
2015-07-10 17:07:04 +0300

    added imagesize for external images (http only)

--- tasks/inline-imgsize.js
+++ tasks/inline-imgsize.js
@@ -2,13 +2,16 @@ module.exports = function(grunt) {
 
     var fs        = require('fs'),
         path      = require('path'),
-        imagesize = require('imagesize');
+        imagesize = require('imagesize'),
+        http = require('http'),
+        Q = require('q');
 
     var regexes = {
         // only match local files
-        img: /<img[^\>]+src=['"](?!http:|https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm,
+        img: /<img[^\>]+src=['"](?!https:|\/\/|data:image)([^"']+)["'][^\>]*>/gm,
         src: /src=['"]([^"']+)["']/m,
-        size: /(height|width)=/
+        size: /(height|width)=/,
+        ext: /(http:)/
     };
 
     var fileOptions = {
@@ -23,18 +26,53 @@ module.exports = function(grunt) {
 
         grunt.util._.extend(options, this.options());
 
+        var done = this.async();
+
         var Parser = imagesize.Parser;
-        var get_image_dimensions = function (buffer) {
+        var get_image_dimensions = function (buffer, tag) {
+            var deferred = Q.defer();
             var parser = Parser();
+
+            var res = {
+                dimensions: '',
+                tag: tag
+            };
 
             switch (parser.parse(buffer)) {
                 case Parser.EOF:
-                    return;
+                    deferred.resolve(false);
                 case Parser.INVALID:
-                    return;
+                    deferred.resolve(false);
                 case Parser.DONE:
-                    return parser.getResult();
+                    res.dimensions = parser.getResult();
+                    deferred.resolve(res);
             }
+            return deferred.promise;
+
+        };
+
+        var get_image_dimersions_external = function(src, tag){
+            var deferred = Q.defer();
+            //var done = this.async();
+
+            var res = {
+                dimensions: '',
+                tag: tag
+            };
+
+            var request = http.get(src, function (response) {
+                imagesize(response, function (err, result) {
+                    // do something with result
+                    res.dimensions = result;
+                    deferred.resolve(res);
+
+                    // we don't need more data
+                    request.abort();
+
+                    //done();
+                });
+            });
+            return deferred.promise;
         };
 
         this.files.forEach(function(f) {
@@ -50,6 +88,8 @@ module.exports = function(grunt) {
                 var contents = grunt.file.read(path, fileOptions);
 
                 var matches = contents.match(regexes.img) || [];
+                var resolved = [];
+
                 matches.forEach(function(tag) {
                     // XXX is this necessary?
                     // tag = tag.substring(0, tag.length - 1);
@@ -59,19 +99,37 @@ module.exports = function(grunt) {
                         return;
                     }
 
-                    var src = tag.match(regexes.src)[1];
-                    var imgpath = path.replace(/[^\/]+$/, '') + src;
-                    var dimensions = get_image_dimensions(fs.readFileSync(imgpath));
-                    if (!dimensions) {
-                        return;
+                    var src = tag.match(regexes.src)[1], dimensions;
+
+                    if (src.match(regexes.ext)) {
+
+                        dimensions = get_image_dimersions_external(src, tag);
+
+                    }else{
+
+                        var imgpath = path.replace(/[^\/]+$/, '') + src;
+                        dimensions = get_image_dimensions(fs.readFileSync(imgpath), tag);
+
                     }
 
-                    var replacement = tag.replace(/^<img/, "<img width=" + dimensions.width + " height=" + dimensions.height);
+                    resolved.push(dimensions);
 
-                    contents = contents.replace(tag, replacement);
                 });
 
-                grunt.file.write(path, contents, fileOptions);
+                Q.all(resolved).then(function(response){
+
+                    response.forEach(function(res){
+                        var replacement = res.tag.replace(/^<img/, "<img width=\"" + res.dimensions.width + "\" height=\"" + res.dimensions.height + "\"");
+                        contents = contents.replace(res.tag, replacement);
+
+                        grunt.file.write(path, contents, fileOptions);
+                    });
+
+                }).done(function(){
+                    done();
+                });
+
+
             });
         });
     });
--- test/inline_imgsize_test.js
+++ test/inline_imgsize_test.js
@@ -5,16 +5,16 @@ exports.inlineImgSize = {
         test.expect(8);
 
         var images = grunt.file.read('tmp/images.html');
-        test.ok(images.match(/<img width=100 height=100 src="assets\/sartak.jpg" alt="avatar">/), 'inline jpg size');
-        test.ok(images.match(/<img width=100 height=100 src="assets\/sartak.jpg" alt="autoclosed" \/>/), 'inline jpg size');
-        test.ok(images.match(/<img width=100 height=100 src="assets\/sartak.gif" alt="gif">/), 'inline gif size');
-        test.ok(images.match(/<img width=100 height=100 src="assets\/sartak.png" alt="png">/), 'inline png size');
+        test.ok(images.match(/<img width="100" height="100" src="assets\/sartak.jpg" alt="avatar">/), 'inline jpg size');
+        test.ok(images.match(/<img width="100" height="100" src="assets\/sartak.jpg" alt="autoclosed" \/>/), 'inline jpg size');
+        test.ok(images.match(/<img width="100" height="100" src="assets\/sartak.gif" alt="gif">/), 'inline gif size');
+        test.ok(images.match(/<img width="100" height="100" src="assets\/sartak.png" alt="png">/), 'inline png size');
 
 
         test.ok(images.match(/<img src="assets\/sartak.jpg" alt="retina" width="50" height="50">/), 'did not overwrite manually-specified size');
         test.ok(images.match(/<img width="50" src="assets\/sartak.jpg" alt="width">/), 'did not overwrite manually-specified size');
         test.ok(images.match(/<img src="assets\/sartak.jpg" height=50 alt="height">/), 'did not overwrite manually-specified size');
-        test.ok(images.match(/<img src="http:\/\/example.com\/remote.jpg" alt="remote">/), 'did not inline size for remote image');
+        test.ok(images.match(/<img width="300" height="110" src="http:\/\/www.google.com\/images\/errors\/logo_sm_2_hr.png" alt="remote">/), 'did not inline size for remote image');
 
         test.done();
     }
